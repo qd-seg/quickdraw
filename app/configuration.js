493311b6af72ca01cfa22cca0cd3333c64d553bb
Daniel Syomichev <dsyo@terpmail.umd.edu>
2024-10-18 14:45:58 -0400

    Added Stage for Building OHIF Viewer

--- app/configuration.js
+++ app/configuration.js
@@ -1,67 +1,54 @@
 /** @type {AppTypes.Config} */
 
 window.config = {
-    routerBasename: '/',
+  routerBasename: "/",
 
-    extensions: [],
-    modes: [],
+  extensions: [],
+  modes: [],
 
-    customizationService: {
-        dicomUploadComponent:
-            '@ohif/extension-cornerstone.customizationModule.cornerstoneDicomUploadComponent',
+  customizationService: {
+    dicomUploadComponent:
+      "@ohif/extension-cornerstone.customizationModule.cornerstoneDicomUploadComponent",
+  },
+
+  showStudyList: true,
+  showWarningMessageForCrossOrigin: true,
+  showCPUFallbackMessage: true,
+  showLoadingIndicator: true,
+
+  strictZSpacingForVolumeViewport: true,
+  useSharedArrayBuffer: false,
+  studyListFunctionsEnabled: true,
+
+  defaultDataSourceName: "orthanc",
+
+  dataSources: [
+    {
+      namespace: "@ohif/extension-default.dataSourcesModule.dicomweb",
+      sourceName: "orthanc",
+
+      configuration: {
+        friendlyName: "Orthanc DICOM Server",
+        name: "Orthanc",
+
+        wadoUriRoot: "/store/wado",
+        qidoRoot: "/store/dicom-web",
+        wadoRoot: "/store/dicom-web",
+
+        imageRendering: "wadors",
+        thumbnailRendering: "wadors",
+
+        supportsFuzzyMatching: true,
+        supportsWildcard: true,
+        supportsReject: true,
+
+        qidoSupportsIncludeField: true,
+        enableStudyLazyLoad: true,
+        dicomUploadEnabled: true,
+        omitQuotationForMultipartRequest: true,
+
+        bulkDataURI: { enabled: true },
+      },
     },
-
-    showWarningMessageForCrossOrigin: true,
-    showCPUFallbackMessage: true,
-    showLoadingIndicator: true,
-    strictZSpacingForVolumeViewport: true,
-    useSharedArrayBuffer: false,
-    studyListFunctionsEnabled: true,
-
-    defaultDataSourceName: 'orthanc',
-
-    dataSources: [
-        {
-            namespace: '@ohif/extension-default.dataSourcesModule.dicomweb',
-            sourceName: 'orthanc',
-            configuration: {
-                friendlyName: 'Orthanc DICOMWeb Server',
-                name: 'Orthanc',
-
-                wadoUriRoot: '/store/wado',
-                qidoRoot: '/store/dicom-web',
-                wadoRoot: '/store/dicom-web',
-
-                qidoSupportsIncludeField: true,
-                supportsReject: true,
-
-                imageRendering: 'wadors',
-                thumbnailRendering: 'wadors',
-
-                enableStudyLazyLoad: true,
-                supportsFuzzyMatching: true,
-                supportsWildcard: true,
-                dicomUploadEnabled: true,
-                omitQuotationForMultipartRequest: true,
-
-                bulkDataURI: { enabled: true },
-            },
-        },
-        {
-            namespace: '@ohif/extension-default.dataSourcesModule.dicomjson',
-            sourceName: 'json',
-            configuration: {
-                friendlyName: 'DICOM JSON',
-                name: 'JSON',
-            },
-        },
-        {
-            namespace: '@ohif/extension-default.dataSourcesModule.dicomlocal',
-            sourceName: 'local',
-            dicomUploadEnabled: true,
-            configuration: {
-                friendlyName: 'DICOM Local',
-            },
-        }
-    ],
+  ],
 };
