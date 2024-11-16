12e1daf5dff1a4715cb065918fa79c2c7e5dff2f
Gopinath, Padmini <padminig@terpmail.umd.edu>
2024-11-15 20:25:49 -0500

    updated seg_converter_main_func.py

--- platform/broker/src/seg_converter_main_func.py
+++ platform/broker/src/seg_converter_main_func.py
@@ -2,14 +2,14 @@ from rtstruct_to_seg_conversion import get_roi_masks, convert_mask_to_dicom_seg,
 
 def process_conversion(dicom_series_path, rt_struct_path, seg_filename):
     # Step 1: Get ROI masks
-    masks = get_roi_masks(dicom_series_path, rt_struct_path)
+    masks, valid_roi_names = get_roi_masks(dicom_series_path, rt_struct_path)
     if masks:
         # Step 2: Load DICOM series
         dicom_series = load_dicom_series(dicom_series_path)
-        roi_names = list(masks.keys())
+        #roi_names = list(masks.keys())
         
         # Step 3: Convert mask to DICOM SEG
-        convert_mask_to_dicom_seg(dicom_series, masks, roi_names, seg_filename)
+        convert_mask_to_dicom_seg(dicom_series, masks, valid_roi_names, seg_filename)
         
         return seg_filename
     else:
