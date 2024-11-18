040304449b01b8b52471466b19b193c407de4b66
Daniel Syomichev <daniel@syomichev.com>
2024-11-18 16:34:04 -0500

    Added the Socket Client

--- platform/viewer/extensions/predict-provisioner/src/components/AnalysisPanel.tsx
+++ platform/viewer/extensions/predict-provisioner/src/components/AnalysisPanel.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { io } from 'socket.io-client';
 import { ProgressLoadingBar, Toolbox } from '@ohif/ui';
 
 import ModelRelationPanelSection from './ModelRelationPanelSection';
@@ -22,6 +23,38 @@ export default ({ servicesManager, commandsManager, extensionManager }) => {
   React.useEffect(() => {
     isProcessing() ? setProgress(undefined) : setProgress(0);
   }, [status]);
+
+  React.useEffect(() => {
+    const { uiNotificationService } = servicesManager.services;
+
+    const socket = io('/api/socket.io');
+
+    socket.on('status_update', ({ message }) => {
+      uiNotificationService.show({
+        title: 'Status Update',
+        message,
+        type: 'info',
+        duration: 5000,
+      });
+    });
+
+    socket.on('progress_update', ({ value }) => {
+      setProgress(value);
+    });
+
+    socket.on('toast_message', ({ type, message }) => {
+      uiNotificationService.show({
+        title: 'Status Update',
+        message,
+        type,
+        duration: 5000,
+      });
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
   return (
     <div>
