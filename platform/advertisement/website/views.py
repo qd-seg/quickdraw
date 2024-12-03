7efe4a5bef8402c116d0cadd9bf7e3af5a4c1895
Daniel Syomichev <daniel@syomichev.com>
2024-12-03 00:44:31 -0500

    Remove Advertisement Site

--- platform/advertisement/website/views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, flash, redirect, render_template, request, url_for
-
-import requests
-views = Blueprint('views', __name__)
-
-@views.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template("index.html")    
-
